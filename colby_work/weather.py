0e2f67f8e760de20b2b4788129d7d693b79d7530
Colby Medeiros <medeiroscolby@gmail.com>
2022-05-04 15:21:35 -0700

    get monterey location from openweathermap geolocation API

--- colby_work/weather.py
+++ colby_work/weather.py
@@ -1,20 +1,33 @@
 import requests
-
+api_key = '5758fb87d0125025a8e357c34f764643'
 def get_monterey_weather():
-    api_key = '5758fb87d0125025a8e357c34f764643'
     endpoint = 'https://api.openweathermap.org/data/2.5/weather'
-
+    coords = get_monterey_coords()
+    
     payload = {
         'appid' : api_key,
         'units' : 'imperial',
-        'lat' : 36.6002,
-        'lon' : -121.8947
+        'lat' : coords['lat'],
+        'lon' : coords['lon']
+    }
+    try:
+        r = requests.get(endpoint, params=payload)
+        data = r.json()
+        return data
+    except:
+        print('failed')
+def get_monterey_coords():
+    endpoint = 'http://api.openweathermap.org/geo/1.0/direct'
+    
+    payload = {
+        'q' : 'Monterey,CA,US',
+        'appid' : api_key,
+        'limit' : 1
     }
 
     try:
         r = requests.get(endpoint, params=payload)
         data = r.json()
-        # print(data)
-        return data
+        return {'lat': data[0]['lat'], 'lon' : data[0]['lon']}
     except:
         print('failed')
