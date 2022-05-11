56ba242fb7f42a2365ad29dda3d0370cf5ec2626
Derek Lilienthal <dlilienth@gmail.com>
2022-05-11 15:05:59 -0700

    Integretated the crypto API into the pet api. But it is not hooked up to the templates yet properly

--- dereks_work/pet_api_main.py
+++ dereks_work/pet_api_main.py
@@ -3,10 +3,12 @@ try:
     from .cat_api import get_cat_url
     from .dog_api import get_dog_url
     from .test_data import cat_test_data, dog_test_data
+    from .crypto_api import get_current_crypto_prices
 except:
     from cat_api import get_cat_url
     from dog_api import get_dog_url
     from test_data import cat_test_data, dog_test_data
+    from crypto_api import get_current_crypto_prices
 
 
 cat_test_data_copy = cat_test_data.copy()
@@ -29,11 +31,15 @@ def get_cat_dog_url_and_prices(size: int, use_test_data=True) -> list(list()):
         print('The most amount of API calls you can use in a single time is 10.\nChanging the size to 10...')
         size = 10
 
+    # Saving the API results to a list
     api_results = []
+
+    # Getting the current crypto prices
+    crypto_prices = get_current_crypto_prices()
 
     # Randomly selects a dog or cat API
     for rand_val in np.random.choice([0,1], size=(size)):
-        cur_api_result = []
+        cur_api_result = {}
         random_pet_price = np.random.randint(1000) # 1000 dollars is the max pet price
         
         # 0 for cat API
@@ -41,24 +47,28 @@ def get_cat_dog_url_and_prices(size: int, use_test_data=True) -> list(list()):
             if use_test_data: # For testing purposes
                 num_random_choice_left = len(cat_test_data_copy)
                 random_test_choice = cat_test_data_copy[np.random.randint(num_random_choice_left)]
-                cur_api_result.append(random_test_choice[0].get('url'))
+                animal_url = random_test_choice[0].get('url')
             else:
                 print('Making cat API call')
-                cur_api_result.append(get_cat_url())
-
-            cur_api_result.append(random_pet_price)
+                animal_url = get_cat_url()
         
         # 1 for dog api call
         if rand_val == 1: 
             if use_test_data:
                 num_random_choice_left = len(dog_test_data_copy)
                 random_test_choice = dog_test_data_copy[np.random.randint(num_random_choice_left)]
-                cur_api_result.append(random_test_choice.get('message'))
+                animal_url = random_test_choice.get('message')
             else:
                 print('Making dog API call')
-                cur_api_result.append(get_dog_url())
+                animal_url = get_dog_url()
 
-            cur_api_result.append(random_pet_price)
+        cur_api_result['url'] = animal_url
+        cur_api_result['USD'] = random_pet_price
+
+        # Converiting the random USD number to crptyo
+        for crypto in crypto_prices:
+            key, val = list(crypto.keys())[0], list(crypto.values())[0]
+            cur_api_result[key] = val * random_pet_price
 
         # Adding the results to the total list    
         api_results.append(cur_api_result)
